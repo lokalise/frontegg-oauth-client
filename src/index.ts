cd29069bfd6331c27195c3b3c4e77076be6e85a8
Ondrej Sevcik <ondrej.sevcik@lokalise.com>
2024-07-24 16:06:04 +0200

    fix: export FrontenggError correctly

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export {
+  FronteggError,
   FronteggOAuthClient,
   type FronteggUserData,
-  type FronteggError,
   type GetFronteggTokenResponse,
   type GetFronteggUserDataResponse,
 } from './frontegg-oauth-client'
